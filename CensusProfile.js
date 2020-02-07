2112178aea3c11efaf6958d350d46fe73cf00f2e
Guillaume Savoie <f.flat.minor@gmail.com>
2020-02-07 16:28:16 -0500

    tabs to spaces

--- CensusProfile.js
+++ CensusProfile.js
@@ -1,18 +1,18 @@
 // REST API documentation :
 // https://www12.statcan.gc.ca/wds-sdw/cpr2016-eng.cfm
 const https = require('https'),
-				 fs = require('fs'),
-			 host = 'https://www12.statcan.gc.ca/',
-			 path = 'rest/census-recensement/CPR2016.json?',
-			 lang = 'lang=E&', geo = '', topic = 'topic=13&', notes = 'notes=0';
-			 // 'dguid=2016A000011124&'; //see constructTheUrl for CSV -> dguid
+         fs = require('fs'),
+       host = 'https://www12.statcan.gc.ca/',
+       path = 'rest/census-recensement/CPR2016.json?',
+       lang = 'lang=E&', geo = '', topic = 'topic=13&', notes = 'notes=0';
+       // 'dguid=2016A000011124&'; //see constructTheUrl for CSV -> dguid
 
 /*
-	convert CSV file to string, and string to array (split the string on new line)
-	the symbol   \n   is a new line character.
-	a CSV file is split in columns with "," and rows with "\n".
-	our geoCSV string will be one column with many rows so
-	we can ignore the "," since GEO.csv has only one column.
+  convert CSV file to string, and string to array (split the string on new line)
+  the symbol   \n   is a new line character.
+  a CSV file is split in columns with "," and rows with "\n".
+  our geoCSV string will be one column with many rows so
+  we can ignore the "," since GEO.csv has only one column.
 */
 let CSV = fs.readFileSync('./GEO.csv');
 let geoCSV = CSV.toString().split("\n"); //this is an array[]
@@ -37,64 +37,64 @@ function constructTheUrl(i) {
 }
 
 (async function() {
-	await makeRequestByPromise();
+  await makeRequestByPromise();
 })();
 async function makeRequestByPromise() {
-	try {
-		for (i = 0; i < gLength; i++) {
-			url = urls[i];
-			let http_promise = getPromise();
-			let response_body = await http_promise;
-		}
-	} catch(error) {
-		console.log(error);
-	}
+  try {
+    for (i = 0; i < gLength; i++) {
+      url = urls[i];
+      let http_promise = getPromise();
+      let response_body = await http_promise;
+    }
+  } catch(error) {
+    console.log(error);
+  }
 }
 function getPromise() {
-	return new Promise((resolve, reject) => {
-		https.get(url, (response) => {
-			let chunks_of_data = [];
-			response.on('data', (fragments) => {
-				chunks_of_data.push(fragments);
-			});
-			response.on('end', () => {
-				let response_body = Buffer.concat(chunks_of_data);
-				let x = responseHandler(response_body.toString());
-				resolve(x);
-			});
-			response.on('error', (error) => {
-				reject(error);
-			});
-		});
-	});
+  return new Promise((resolve, reject) => {
+    https.get(url, (response) => {
+      let chunks_of_data = [];
+      response.on('data', (fragments) => {
+        chunks_of_data.push(fragments);
+      });
+      response.on('end', () => {
+        let response_body = Buffer.concat(chunks_of_data);
+        let x = responseHandler(response_body.toString());
+        resolve(x);
+      });
+      response.on('error', (error) => {
+        reject(error);
+      });
+    });
+  });
 }
 function responseHandler(res) {
-	let trimMsg = res.slice(2); //remove first two characters
-	let obj     = JSON.parse(trimMsg);//string to JS object
-	let geoName = obj.DATA[0][4];
-	let geoID   = obj.DATA[0][2];
-	let density = obj.DATA[5][13];
-	let landArea = obj.DATA[6][13];
-	let fetchedData = [geoName, geoID, density, landArea];
-	
-	accString += fetchedData.join() + "\n";
-	
-	if (counter === gLength -1){
-		areWeThereYet = true;
-	} else {
-		counter = counter + 1
-	}
-	if (areWeThereYet) {
-		output(accString);
-	}
+  let trimMsg = res.slice(2); //remove first two characters
+  let obj     = JSON.parse(trimMsg);//string to JS object
+  let geoName = obj.DATA[0][4];
+  let geoID   = obj.DATA[0][2];
+  let density = obj.DATA[5][13];
+  let landArea = obj.DATA[6][13];
+  let fetchedData = [geoName, geoID, density, landArea];
+  
+  accString += fetchedData.join() + "\n";
+  
+  if (counter === gLength -1){
+    areWeThereYet = true;
+  } else {
+    counter = counter + 1
+  }
+  if (areWeThereYet) {
+    output(accString);
+  }
 }
 
 function output(_this) {
-	fs.writeFile("./GET.csv", _this, function(err) {
-		if (err) {
-			return console.log("look at this error: " + err);
-		} else {
-			return console.log("Looks like it worked...");
-		}
-	});
+  fs.writeFile("./GET.csv", _this, function(err) {
+    if (err) {
+      return console.log("look at this error: " + err);
+    } else {
+      return console.log("Looks like it worked...");
+    }
+  });
 }
--- GEO.js
+++ GEO.js
@@ -1,64 +1,64 @@
 const https = require('https'),
          fs = require('fs'),
-			 host = 'https://www12.statcan.gc.ca/', 
+       host = 'https://www12.statcan.gc.ca/', 
        path = 'rest/census-recensement/CR2016Geo.json?',
        lang = 'lang=E&', geo  = 'geos=PR&', cpt  = 'cpt=00',
         url = host + path + lang + geo + cpt;
        // 24 = Québec
       // All Census Subdivisions (csd)
 (async function() {
-	await makeRequestByPromise();
+  await makeRequestByPromise();
 })();
 async function makeRequestByPromise() {
-	try {
-		let http_promise = getPromise();
-		let response_body = await http_promise;
-		console.log(response_body.length);
-		console.log(response_body);
-		output(response_body.join(""));
-	} catch(error) {
-		console.log(error);
-	}
+  try {
+    let http_promise = getPromise();
+    let response_body = await http_promise;
+    console.log(response_body.length);
+    console.log(response_body);
+    output(response_body.join(""));
+  } catch(error) {
+    console.log(error);
+  }
 }
 function getPromise() {
-	return new Promise((resolve, reject) => {
-		https.get(url, (response) => {
-			let chunks_of_data = [];
-			response.on('data', (fragments) => {
-				chunks_of_data.push(fragments);
-			});
-			response.on('end', () => {
-				let response_body = Buffer.concat(chunks_of_data);
-				let x = responseHandler(response_body.toString());
-				resolve(x);
-			});
-			response.on('error', (error) => {
-				reject(error);
-			});
-		});
-	});
+  return new Promise((resolve, reject) => {
+    https.get(url, (response) => {
+      let chunks_of_data = [];
+      response.on('data', (fragments) => {
+        chunks_of_data.push(fragments);
+      });
+      response.on('end', () => {
+        let response_body = Buffer.concat(chunks_of_data);
+        let x = responseHandler(response_body.toString());
+        resolve(x);
+      });
+      response.on('error', (error) => {
+        reject(error);
+      });
+    });
+  });
 }
 function responseHandler(res) {
-	let trimMsg = res.slice(2);//remove first two characters
-	let p = JSON.parse(trimMsg);//transform string into JS object
-	//console.log(p.DATA[0][0]);
-	let list = [];
-	let pLength = p.DATA.length;
-	for (let i = 0; i < pLength; i++){
-		if (i !== pLength - 1){
-			list[i] = p.DATA[i][0] + "\n"; //grab dguid only
-		} else {
-			list[i] = p.DATA[i][0];
-		}
-	}
-	return list;
+  let trimMsg = res.slice(2);//remove first two characters
+  let p = JSON.parse(trimMsg);//transform string into JS object
+  //console.log(p.DATA[0][0]);
+  let list = [];
+  let pLength = p.DATA.length;
+  for (let i = 0; i < pLength; i++){
+    if (i !== pLength - 1){
+      list[i] = p.DATA[i][0] + "\n"; //grab dguid only
+    } else {
+      list[i] = p.DATA[i][0];
+    }
+  }
+  return list;
 }
 function output(_this) {
-	fs.writeFile("./GEO.csv", _this, function(err) {
-		if (err) {
-			return console.log(err);
-		} else {
-			return console.log("Looks like it worked...(list)");
-		}
-	});
+  fs.writeFile("./GEO.csv", _this, function(err) {
+    if (err) {
+      return console.log(err);
+    } else {
+      return console.log("Looks like it worked...(list)");
+    }
+  });
 }
