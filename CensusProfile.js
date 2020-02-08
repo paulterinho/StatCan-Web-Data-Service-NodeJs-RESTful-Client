2b8ab5caa70f31df7328ef5de12807b791360254
Guillaume Savoie <f.flat.minor@gmail.com>
2020-02-07 20:52:36 -0500

    Census profile works! Need2test larger request...

--- CensusProfile.js
+++ CensusProfile.js
@@ -1,56 +1,51 @@
 // REST API documentation :
 // https://www12.statcan.gc.ca/wds-sdw/cpr2016-eng.cfm
 const https = require('https'),
-         fs = require('fs'),
+         fs = require('fs'), // file system
        host = 'https://www12.statcan.gc.ca/',
        path = 'rest/census-recensement/CPR2016.json?',
-       lang = 'lang=E&', geo = '', topic = 'topic=13&', notes = 'notes=0';
-       // 'dguid=2016A000011124&'; //see constructTheUrl for CSV -> dguid
+       lang = 'lang=E&', geo = '', topic = 'topic=13&', notes = 'notes=0',
+				CSV = fs.readFileSync('./GEO.csv'),
+     geoCSV = CSV.toString().split("\n"), //this is an array[]
+		gLength = geoCSV.length;
 
-/*
-  convert CSV file to string, and string to array (split the string on new line)
-  the symbol   \n   is a new line character.
-  a CSV file is split in columns with "," and rows with "\n".
-  our geoCSV string will be one column with many rows so
-  we can ignore the "," since GEO.csv has only one column.
-*/
-let CSV = fs.readFileSync('./GEO.csv');
-let geoCSV = CSV.toString().split("\n"); //this is an array[]
-console.log(geoCSV);
-let url;
-let areWeThereYet = false;
+console.log("Fetching data for " + gLength + " geographic units");
 
-let accString = ""; //accumulate responses in one long string
-let urls      = []; // my list of requests to make (http GET)
-let counter   = 0;
-let gLength   = geoCSV.length;
-// put each dguid into the urls[] array. 
-for (i = 0; i < gLength; i++) {
-  urls[i] = constructTheUrl(i);
-}
+let	counter = 0,
+			 urls = [], // my list of requests to make (http GET)
+	accString = ""; // accumulate responses in one long string
 
-// make a request for each url and store response in accString
-
-function constructTheUrl(i) {
-  let url = host + path + lang + "dguid=" + geoCSV[i] + "&" + topic + notes;
-  return url;
-}
+(function makeUrlsArray() {
+	// put each dguid into the urls[] array. 
+	for (i = 0; i < gLength; i += 1) {
+		urls[i] = constructTheUrl(i);
+	}
+	// make a request for each url and store response in accString
+	function constructTheUrl(i) {
+		let url = host + path + lang + "dguid=" + geoCSV[i] + "&" + topic + notes;
+		return url;
+	}
+})();
 
 (async function() {
+	console.log("starting...");
   await makeRequestByPromise();
+	console.log("finished!");
 })();
+
 async function makeRequestByPromise() {
   try {
     for (i = 0; i < gLength; i++) {
-      url = urls[i];
-      let http_promise = getPromise();
+      let url = urls[i];
+      let http_promise = getPromise(url);
       let response_body = await http_promise;
     }
   } catch(error) {
     console.log(error);
   }
 }
-function getPromise() {
+
+function getPromise(url) {
   return new Promise((resolve, reject) => {
     https.get(url, (response) => {
       let chunks_of_data = [];
@@ -68,33 +63,25 @@ function getPromise() {
     });
   });
 }
+
 function responseHandler(res) {
-  let trimMsg = res.slice(2); //remove first two characters
-  let obj     = JSON.parse(trimMsg);//string to JS object
-  let geoName = obj.DATA[0][4];
-  let geoID   = obj.DATA[0][2];
-  let density = obj.DATA[5][13];
-  let landArea = obj.DATA[6][13];
-  let fetchedData = [geoName, geoID, density, landArea];
+  let trimMsg = res.slice(2), // remove first two characters
+					obj = JSON.parse(trimMsg), // string to JS object
+			geoName = obj.DATA[0][4],
+				geoID = obj.DATA[0][2],
+			density = obj.DATA[5][13],
+		 landArea = obj.DATA[6][13],
+  fetchedData = [geoName, geoID, density, landArea];
   
   accString += fetchedData.join() + "\n";
-  
-  if (counter === gLength -1){
-    areWeThereYet = true;
-  } else {
-    counter = counter + 1
-  }
-  if (areWeThereYet) {
-    output(accString);
-  }
+	
+	counter === gLength - 1 ? output(accString) : counter += 1;
+	
+	function output(_this) {
+		let s = "./GET.csv";
+		fs.writeFile(s, _this, function(err) {
+			err ? console.log("Look!" + err) : console.log("File written as " + s);
+		});
+	}
 }
 
-function output(_this) {
-  fs.writeFile("./GET.csv", _this, function(err) {
-    if (err) {
-      return console.log("look at this error: " + err);
-    } else {
-      return console.log("Looks like it worked...");
-    }
-  });
-}
