97ac2231cf3abba1a9022b09c7a0c581a122b60e
Guillaume Savoie <f.flat.minor@gmail.com>
2020-02-08 09:30:06 -0500

    tabs to spaces

--- simplePost.js
+++ simplePost.js
@@ -1,30 +1,30 @@
 "use strict";
 const https = require('https'),
-				url = '/t1/wds/rest/getSeriesInfoFromVector',
-					v = '1',
-			 data =  '[{\"vectorId\":' + v + '}]',
-		options = {
-			hostname: 'www150.statcan.gc.ca',
-			port: 443,
-			path: url,
-			method: 'POST',
-			headers: {
-				'Content-Type': 'application/json',
-				'Content-Length': data.length
-			}
-		};
+        url = '/t1/wds/rest/getSeriesInfoFromVector',
+          v = '1',
+       data =  '[{\"vectorId\":' + v + '}]',
+    options = {
+      hostname: 'www150.statcan.gc.ca',
+      port: 443,
+      path: url,
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Content-Length': data.length
+      }
+    };
 let req = https.request(options, (res) => {
-	let chunks_of_data = [];
-	res.on('data', (fragments) => {
-		chunks_of_data.push(fragments);
-	});
-	res.on('end', () => {
-		let response_body = Buffer.concat(chunks_of_data);
-		console.log(JSON.parse(response_body.toString()));
-	});
+  let chunks_of_data = [];
+  res.on('data', (fragments) => {
+    chunks_of_data.push(fragments);
+  });
+  res.on('end', () => {
+    let response_body = Buffer.concat(chunks_of_data);
+    console.log(JSON.parse(response_body.toString()));
+  });
 });
 req.on('error', error => {
-	console.log(error);
+  console.log(error);
 });
 req.write(data);
 req.end();
